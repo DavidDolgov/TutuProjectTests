3075a68ceceb17973da0205fba482ee3914c4df5
Dolgov David <dolgov.david.1993@gmail.com>
2025-07-03 12:14:19 +0300

    errors.fix

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,17 +1,21 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import lombok.Getter;
 
+import java.util.stream.IntStream;
 
-import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
 //вся логика с элементами на главноя страницы tutu.ru
+@Getter
 public class MainPage {
     //все поля для ввода информации
-    private final ElementsCollection inputFields = $$x("//input[@placeholder=' ']");
+    private final SelenideElement inputFieldFromWhere = $x("//div//span[contains(text(),'Откуда')]/../input[@placeholder=' ']");
+    private final SelenideElement inputFieldWhere = $x("//div//span[contains(text(),'Куда')]/../input[@placeholder=' ']");
+    private final SelenideElement inputFieldWhen = $x("//div//span[contains(text(),'Когда')]/../input[@placeholder=' ']");
+    private final SelenideElement inputFieldBack = $x("//div//span[contains(text(),'Обратно')]/../input[@placeholder=' ']");
+    private final SelenideElement inputFieldWhoIsFlying = $x("//div//span[contains(text(),'Кто летит')]/../input[@placeholder=' ']");
 
     //кнопки отчиски и принятия относительно даты
     private final SelenideElement selectButton = $x("//button[@variant='primary']");
@@ -25,13 +29,9 @@ public class MainPage {
     private final SelenideElement submitButton = $x("//button[@data-ti='submit-button']");
 
     //Элементы по ошибкам заполнения формы
-    @Getter
     private final SelenideElement errorThatFieldWhereIsEmpty = $x("//div[@data-ti='tooltip-content']/span[contains(text(),'Выберите город прилёта')]");
-    @Getter
     private final SelenideElement errorThatFieldFromIsEmpty = $x("//div[@data-ti='tooltip-content']/span[contains(text(),'Выберите город вылета')]");
-    @Getter
     private final SelenideElement errorThatFieldsFromAndToAreIdentical = $x("//div[@data-ti='tooltip-content']/span[contains(text(),'должны совпадать')]");
-    @Getter
     private final SelenideElement errorThatDepartureDateFieldIsEmpty = $x("//div[@data-ti='tooltip-content']/span[contains(text(),'Выберите дату вылета')]");
 
 
@@ -40,40 +40,37 @@ public class MainPage {
     }
 
     public void setValueInFieldFromWhere(String text) {
-        inputFields.get(0).sendKeys(text);
+        inputFieldFromWhere.sendKeys(text);
         $x("//div[@data-ti='dropdown-item']//*[contains(text(),'" + text + "')]")
                 .shouldBe(Condition.visible).click();
     }
 
     public void setValueInFieldWhere(String text) {
-        inputFields.get(1).setValue(text);
+        inputFieldWhere.sendKeys(text);
         $x("//div[@data-ti='dropdown-item']//*[contains(text(),'" + text + "')]")
                 .shouldBe(Condition.visible).click();
     }
 
     public void setValueInFieldWhen(String data) {
-        inputFields.get(2).click();
+        inputFieldWhen.click();
         $x("//div[@class='DayPicker-Day'] [contains(@aria-label,'" + data + "')]")
                 .shouldBe(Condition.visible).click();
     }
 
     public void setValueInFieldBack(String data) {
-        inputFields.get(3).click();
+        inputFieldBack.click();
         $x("//div[@class='DayPicker-Day'] [contains(@aria-label,'" + data + "')]")
                 .shouldBe(Condition.visible).click();
         selectButton.click();
     }
 
     public void setValueInFieldWhoIsFlying(int adultPassengers, boolean economy) {
-        inputFields.get(4).click();
+        inputFieldWhoIsFlying.click();
         SelenideElement economyOrBusiness = economy ? economyButton : businessButton;
         economyOrBusiness.shouldBe(Condition.visible).click();
 
-        int count = 1;
-        while (count < adultPassengers) {
-            plusPassengerButton.click();
-            count++;
-        }
+        IntStream stream = IntStream.range(1,adultPassengers);
+        stream.forEach(i -> plusPassengerButton.click());
     }
 
     public TicketSearchResultPage findTickets() {
